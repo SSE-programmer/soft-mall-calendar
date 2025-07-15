fb689459d9cfa79725a056c86e823b2f0fa80d5e
Stanislav Stolbennikov <stolbennikovilmk@gmail.com>
2025-07-15 15:28:04 +0800

    Created base-date.vue

---
+++ src/pages/calendar-view/components/base-date.vue
@@ -0,0 +1,24 @@
+<script
+    lang="ts"
+    setup
+>
+import { computed } from 'vue';
+import { format } from 'date-fns';
+
+interface Props {
+    date: Date | number
+}
+
+const props = defineProps<Props>();
+const month = computed(() => format(props.date, 'MMMM'));
+const year = computed(() => format(props.date, 'yyyy'));
+</script>
+
+<template>
+    <div class="sm-flex sm-flex sm-gap-4">
+        <span class="sm-text-26 sm-font-bold">{{ month }}</span>
+        <span class="sm-text-26 sm-font-bold sm-light-primary-color">{{ year }}</span>
+    </div>
+</template>
+
+<style scoped></style>
