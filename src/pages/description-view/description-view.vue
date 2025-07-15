17dd4707516b7b3d292a3cdeb4f2469e41631f2b
Stanislav Stolbennikov <stolbennikovilmk@gmail.com>
2025-07-15 15:26:51 +0800

    Changed description-view.vue

--- src/pages/description-view/description-view.vue
+++ src/pages/description-view/description-view.vue
@@ -5,7 +5,7 @@
 </script>
 
 <template>
-    <div class="sm-flex sm-flex-col sm-gap-12">
+    <div class="sm-flex sm-flex-col sm-gap-12 sm-flex-1 sm-p-16 sm-bg-light-background sm-border-radius-16 sm-overflow-hidden">
         <p class="sm-font-bold">Требуется реализовать календарь сотрудника с тремя с функциями:</p>
         <ol class="sm-ml-12">
             <li>1. Функция создания задания</li>
