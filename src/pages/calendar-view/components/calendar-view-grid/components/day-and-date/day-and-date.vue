4a0a523619a097cc81aa2e253f9af84cbb72687b
Stanislav Stolbennikov <stolbennikovilmk@gmail.com>
2025-07-16 17:44:15 +0800

    Created day-and-date.vue

---
+++ src/pages/calendar-view/components/calendar-view-grid/components/day-and-date/day-and-date.vue
@@ -0,0 +1,52 @@
+<script
+    setup
+    lang="ts"
+>
+import { formatDate } from 'date-fns';
+
+interface Props {
+    date: Date | number;
+    isToday?: boolean;
+    isOtherMonth?: boolean;
+    isWeekend?: boolean;
+}
+
+const props = defineProps<Props>();
+</script>
+
+<template>
+    <div
+        class="date sm-flex sm-gap-8 sm-items-center sm-justify-center sm-p-8 sm-border-radius-16"
+        :class="{
+            'is-today': props.isToday,
+            'is-other-month': props.isOtherMonth,
+            'is-weekend': props.isWeekend,
+        }"
+    >
+        <span class="sm-font-bold">{{ formatDate(props.date, 'EEEEEE') }}</span>
+        <span>{{ formatDate(props.date, 'd') }}</span>
+    </div>
+</template>
+
+<style scoped>
+.date {
+    --weekend-color: #FF3F3F;
+    --disabled-weekend-color: #ff7f7f;
+}
+
+.is-today {
+    background: var(--sm-gray-middle-color);
+}
+
+.is-weekend {
+    color: var(--weekend-color);
+}
+
+.is-other-month {
+    color: var(--sm-ultra-light-primary-color);
+
+    &.is-weekend {
+        color: var(--disabled-weekend-color);
+    }
+}
+</style>
