574bf807a88340b8068822dc736864f8a08e4d8b
Stanislav Stolbennikov <stolbennikovilmk@gmail.com>
2025-07-16 13:39:45 +0800

    Created pinia instance

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,13 @@ import App from './app.vue';
 import router from './pages/router.ts';
 import PrimeVue from 'primevue/config';
 import Aura from '@primeuix/themes/aura';
+import { createPinia } from 'pinia';
 
 const app = createApp(App);
+const pinia = createPinia();
 
 app.use(router);
+app.use(pinia);
 app.use(PrimeVue, {
     theme: {
         preset: Aura
