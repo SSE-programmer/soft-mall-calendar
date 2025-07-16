e11c75ec26548c02f8d37607190136653696ad3e
Stanislav Stolbennikov <stolbennikovilmk@gmail.com>
2025-07-17 03:15:50 +0800

    Added upload and download icons

---
+++ src/shared/components/icons/icon-download.vue
@@ -0,0 +1,40 @@
+<template>
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="100%"
+        height="100%"
+        viewBox="0 0 24 24"
+    >
+        <path
+            d="M3,12.3v7a2,2,0,0,0,2,2H19a2,2,0,0,0,2-2v-7"
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+        />
+
+        <polyline
+            data-name="Right"
+            fill="none"
+            id="Right-2"
+            points="7.9 12.3 12 16.3 16.1 12.3"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+        />
+
+        <line
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            x1="12"
+            x2="12"
+            y1="2.7"
+            y2="14.2"
+        />
+    </svg>
+</template>
---
+++ src/shared/components/icons/icon-upload.vue
@@ -0,0 +1,41 @@
+<template>
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="100%"
+        height="100%"
+        viewBox="0 0 24 24"
+    >
+        <path
+            d="M3,12.3v7a2,2,0,0,0,2,2H19a2,2,0,0,0,2-2v-7"
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+        />
+
+        <polyline
+            data-name="Right"
+            fill="none"
+            id="Right-2"
+            points="7.9 6.7 12 2.7 16.1 6.7"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+        />
+
+        <line
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            x1="12"
+            x2="12"
+            y1="16.3"
+            y2="4.8"
+        />
+
+    </svg>
+</template>
