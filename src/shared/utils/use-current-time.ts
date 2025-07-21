57ae75b084d4f90cb7a23a9707c4d849b4843c9c
Stanislav Stolbennikov <stolbennikovilmk@gmail.com>
2025-07-22 00:51:37 +0700

    Improved current time hook logic

--- src/shared/utils/use-current-time.ts
+++ src/shared/utils/use-current-time.ts
@@ -1,18 +1,61 @@
 import { onMounted, onUnmounted, ref } from 'vue';
 
-export function useCurrentTime(updateInterval = 60 * 1000) {
+type UpdateInterval = 'second' | 'minute' | 'hour';
+
+const MILLISECONDS_IN_SECOND = 1000;
+const SECONDS_IN_MINUTE = 60;
+const MINUTES_IN_HOUR = 60;
+
+interface NextUpdateIntervalStrategy {
+    getNextInterval(): number;
+}
+
+class SecondUpdateIntervalStrategy implements NextUpdateIntervalStrategy {
+    public getNextInterval(): number {
+        const date = new Date();
+
+        return SECONDS_IN_MINUTE - date.getSeconds();
+    }
+}
+
+class HourUpdateIntervalStrategy implements NextUpdateIntervalStrategy {
+    public getNextInterval(): number {
+        const date = new Date();
+
+        return MINUTES_IN_HOUR - date.getMinutes();
+    }
+}
+
+class MinuteUpdateIntervalStrategy implements NextUpdateIntervalStrategy {
+    public getNextInterval(): number {
+        const date = new Date();
+
+        return MILLISECONDS_IN_SECOND - date.getMilliseconds();
+    }
+}
+
+export function useCurrentTime(updateIntervalStrategy: NextUpdateIntervalStrategy = new MinuteUpdateIntervalStrategy()) {
     const currentTime = ref(new Date());
-    let intervalId: number;
+    let timeoutId: number;
 
     onMounted(() => {
-        intervalId = setInterval(() => {
-            currentTime.value = new Date();
-        }, updateInterval);
+        timeout();
     });
 
     onUnmounted(() => {
-        clearInterval(intervalId);
+        clearTimeout(timeoutId);
     });
 
     return { currentTime };
+
+    function timeout() {
+        timeoutId = setTimeout(() => {
+            updateCurrentTime();
+            timeout();
+        }, updateIntervalStrategy.getNextInterval());
+    }
+
+    function updateCurrentTime() {
+        currentTime.value = new Date();
+    }
 }
