ab471962b54137bbe86ae3ce5909f16939fe54cf
Stanislav Stolbennikov <stolbennikovilmk@gmail.com>
2025-07-23 01:49:28 +0700

    Translated the requirements into English

--- src/pages/description-view/description-view.vue
+++ src/pages/description-view/description-view.vue
@@ -6,11 +6,11 @@
 
 <template>
     <div class="sm-flex sm-flex-col sm-gap-12 sm-flex-1 sm-p-16 sm-bg-light-background sm-border-radius-16 sm-overflow-hidden">
-        <p class="sm-font-bold">Требуется реализовать календарь сотрудника с тремя с функциями:</p>
+        <p class="sm-font-bold">An employee calendar needs to be implemented with three core functions:</p>
         <ol class="sm-ml-12">
-            <li>1. Функция создания задания</li>
-            <li>2. Функция загрузки задания из JSON</li>
-            <li>3. Функция выгрузки задания в JSON</li>
+            <li>1. Task creation functionality</li>
+            <li>2. Task import from JSON</li>
+            <li>3. Task export to JSON</li>
         </ol>
     </div>
 </template>
